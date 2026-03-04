c2cbcb65ff0a2164c8b4bf46dbdd31fae1c6df13
adirathodd <88114611+adirathodd@users.noreply.github.com>
2026-03-04 08:51:53 -0500

    done

---
+++ README.md
@@ -0,0 +1,1 @@
+![Preview](./demo.gif)
--- app/src/main/java/com/codepath/lab6/MainActivity.kt
+++ app/src/main/java/com/codepath/lab6/MainActivity.kt
@@ -1,17 +1,10 @@
 package com.codepath.lab6
 
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.codepath.lab6.databinding.ActivityMainBinding
-import com.codepath.asynchttpclient.AsyncHttpClient
-import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import kotlinx.serialization.json.Json
-import okhttp3.Headers
-import org.json.JSONException
 
 // Helper function for JSON parsing
 fun createJson() = Json {
@@ -20,14 +13,8 @@ fun createJson() = Json {
     useAlternativeNames = false
 }
 
-private const val TAG = "MainActivity/"
-private val API_KEY = BuildConfig.API_KEY
-private val PARKS_URL = "https://developer.nps.gov/api/v1/parks?api_key=${API_KEY}"
-
 class MainActivity : AppCompatActivity() {
 
-    private val parks = mutableListOf<Park>()
-    private lateinit var parksRecyclerView: RecyclerView
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,47 +22,33 @@ class MainActivity : AppCompatActivity() {
 
         // Set up ViewBinding
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        // Find RecyclerView and set up adapter
-        parksRecyclerView = findViewById(R.id.parks)
-        val parksAdapter = ParksAdapter(this, parks)
-        parksRecyclerView.adapter = parksAdapter
-
-        // Use LinearLayoutManager with a divider
-        parksRecyclerView.layoutManager = LinearLayoutManager(this).also {
-            val dividerItemDecoration = DividerItemDecoration(this, it.orientation)
-            parksRecyclerView.addItemDecoration(dividerItemDecoration)
+        // Show Parks fragment by default
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, ParksFragment.newInstance())
+                .commit()
         }
 
-        // Fetch data from NPS Parks API
-        val client = AsyncHttpClient()
-        client.get(PARKS_URL, object : JsonHttpResponseHandler() {
-            override fun onFailure(
-                statusCode: Int,
-                headers: Headers?,
-                response: String?,
-                throwable: Throwable?
-            ) {
-                Log.e(TAG, "Failed to fetch parks: $statusCode")
-            }
-
-            override fun onSuccess(statusCode: Int, headers: Headers, json: JSON) {
-                Log.i(TAG, "Successfully fetched parks: $json")
-                try {
-                    val parsedJson = createJson().decodeFromString(
-                        ParksResponse.serializer(),
-                        json.jsonObject.toString()
-                    )
-                    parsedJson.data?.let { list ->
-                        parks.addAll(list)
-                        parksAdapter.notifyDataSetChanged()
-                    }
-                } catch (e: JSONException) {
-                    Log.e(TAG, "Exception: $e")
+        // Set up Bottom Navigation
+        val bottomNavigation: BottomNavigationView = binding.bottomNavigation
+        bottomNavigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.action_parks -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, ParksFragment.newInstance())
+                        .commit()
+                    true
                 }
+                R.id.action_campgrounds -> {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.fragment_container, CampgroundFragment.newInstance())
+                        .commit()
+                    true
+                }
+                else -> false
             }
-        })
+        }
     }
 }
